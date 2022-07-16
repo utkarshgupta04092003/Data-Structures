e00eca7052dcd499f8ebd65cdf7c2104f71bad8d
Utkarsh Gupta <63789702+utkarshgupta04092003@users.noreply.github.com>
2022-07-16 23:28:25 +0530

    Create Main.java

---
+++ Stack/implement/Main.java
@@ -0,0 +1,65 @@
+// Implement stack using Array
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		Stack st =  new Stack();
+		
+		System.out.println(st.isEmpty());
+		System.out.println(st.size());
+        System.out.println();
+        
+        
+		st.push(5);
+		System.out.println(st.size());
+		System.out.println(st.isEmpty());
+		System.out.println(st.top());
+		System.out.println();
+		
+		st.push(10);
+		System.out.println(st.size());
+		System.out.println(st.isEmpty());
+		System.out.println(st.top());
+		System.out.println();
+
+		
+		System.out.println(st.pop());
+		System.out.println(st.size());
+		System.out.println(st.isEmpty());
+		System.out.println(st.top());
+		System.out.println();
+		
+		System.out.println(st.pop());
+		System.out.println(st.size());
+		System.out.println(st.isEmpty());
+		System.out.println(st.top());
+		System.out.println();
+
+		// stack is empty till here
+		
+
+	    // we can store as much as we want with the help of resizing
+		st.push(1);
+		st.push(2);
+		st.push(3);
+		st.push(4);
+		st.push(5);
+		st.push(6);
+		st.push(7);
+		st.push(8);
+		st.push(9);
+		st.push(10);
+        st.push(11);
+
+
+	    while(!st.isEmpty()){
+		    System.out.print(st.pop()+" ");
+		}
+		System.out.println();
+		
+		
+	}
+}
